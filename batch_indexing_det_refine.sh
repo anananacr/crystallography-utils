3b5d9eb0acdfa3547043db38ec52d2627f6e673b
Ana Rodrigues <anananacr@gmail.com>
2024-04-26 11:54:14 +0200

    chore: testing pink parameters

--- batch_indexing_det_refine.sh
+++ batch_indexing_det_refine.sh
@@ -17,11 +17,12 @@ ROOT=/gpfs/cfel/group/cxi/scratch/2021/ESRF-2024-Meents-Mar-ID09/processed/rodri
 
 source /etc/profile.d/modules.sh
 module purge
-module load maxwell crystfel/0.10.2
+module load maxwell crystfel/0-devel
 
-command="indexamajig -i ${ROOT}/lists/${INPUT} -p ${ROOT}/cell/lyso.cell -o ${ROOT}/streams/${OUTPUT} -g ${ROOT}/geoms/jungfrau-1m-vertical-v1-70mm-predrefine.geom --peaks=peakfinder8"
-command="$command -j 64 --threshold=0 --min-snr=5 --min-pix-count=2 --max-pix-count=200 --min-res=0 --max-res=1200 --min-peaks=10 --int-radius=4,5,6 --copy-header=/entry/shots/detector_shift_x_in_mm --copy-header=/entry/shots/detector_shift_y_in_mm --copy-header=/entry/data/raw_file_id"
-#command="$command --mille --mille-dir=${ROOT}/indexing/mille"
+command="indexamajig -i ${ROOT}/lists/${INPUT} -o ${ROOT}/streams/${OUTPUT} -g ${ROOT}/geoms/jungfrau-1m-vertical-v1-100mm-shift.geom --peaks=peakfinder8"
+command="$command -j 64 --threshold=0 --min-snr=5 --min-pix-count=2 --max-pix-count=200 --min-res=0 --max-res=650 --min-peaks=10 --int-radius=5,6,7 --copy-header=/entry/data/raw_file_id"
+command="$command -p ${ROOT}/cell/lyso.cell "
+#command="$command --indexing=mosflm"
 echo $command
 $command
  
