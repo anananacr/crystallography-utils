c8b8003061a068da26bdc274f6ddf608582390e6
Ana Rodrigues <anananacr@gmail.com>
2024-04-26 11:54:25 +0200

    feat: batch convert EP

--- convert_burst_runs.sh
+++ convert_burst_runs.sh
@@ -1,17 +1,71 @@
 #!/bin/sh
 
-DARKS_D0=pedal_d0_20240308_dark_fixg1_sc_1us_5.8us.h5
-DARKS_D1=pedal_d1_20240308_dark_fixg1_sc_1us_5.8us.h5
+DARKS_D0=pedal_d0_1us_8us_2.h5
+DARKS_D1=pedal_d1_1us_8us_2.h5
 
-sbatch convert_images_burst.sh FAKP_apo_sweep_03/run0001_sc_1us_5.8us_12_master_0.h5 $DARKS_D0 $DARKS_D1;
-sbatch convert_images_burst.sh FAKP_apo_sweep_03/run0001_sc_1us_5.8us_11_master_0.h5 $DARKS_D0 $DARKS_D1;
-sbatch convert_images_burst.sh FAKP_apo_sweep_03/run0001_sc_1us_5.8us_10_master_0.h5 $DARKS_D0 $DARKS_D1;
-sbatch convert_images_burst.sh FAKP_apo_sweep_03/run0001_sc_1us_5.8us_09_master_0.h5 $DARKS_D0 $DARKS_D1;
-sbatch convert_images_burst.sh FAKP_apo_sweep_03/run0001_sc_1us_5.8us_08_master_0.h5 $DARKS_D0 $DARKS_D1;
-sbatch convert_images_burst.sh FAKP_apo_sweep_03/run0001_sc_1us_5.8us_07_master_0.h5 $DARKS_D0 $DARKS_D1;
-sbatch convert_images_burst.sh FAKP_apo_sweep_03/run0001_sc_1us_5.8us_06_master_0.h5 $DARKS_D0 $DARKS_D1;
-sbatch convert_images_burst.sh FAKP_apo_sweep_03/run0001_sc_1us_5.8us_05_master_0.h5 $DARKS_D0 $DARKS_D1;
-sbatch convert_images_burst.sh FAKP_apo_sweep_03/run0001_sc_1us_5.8us_04_master_0.h5 $DARKS_D0 $DARKS_D1;
-sbatch convert_images_burst.sh FAKP_apo_sweep_03/run0001_sc_1us_5.8us_03_master_0.h5 $DARKS_D0 $DARKS_D1;
-sbatch convert_images_burst.sh FAKP_apo_sweep_03/run0001_sc_1us_5.8us_02_master_0.h5 $DARKS_D0 $DARKS_D1;
-sbatch convert_images_burst.sh FAKP_apo_sweep_03/run0001_sc_1us_5.8us_01_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_apo_03_sweep/run0001_sc_1us_5.8us_12_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_apo_03_sweep/run0001_sc_1us_5.8us_11_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_apo_03_sweep/run0001_sc_1us_5.8us_10_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_apo_03_sweep/run0001_sc_1us_5.8us_09_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_apo_03_sweep/run0001_sc_1us_5.8us_08_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_apo_03_sweep/run0001_sc_1us_5.8us_07_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_apo_03_sweep/run0001_sc_1us_5.8us_06_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_apo_03_sweep/run0001_sc_1us_5.8us_05_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_apo_03_sweep/run0001_sc_1us_5.8us_04_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_apo_03_sweep/run0001_sc_1us_5.8us_03_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_apo_03_sweep/run0001_sc_1us_5.8us_02_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_apo_03_sweep/run0001_sc_1us_5.8us_01_master_0.h5 $DARKS_D0 $DARKS_D1;
+
+sbatch convert_images_burst.sh EP_DMSO_01_sweep/run0001_sc_1us_5.8us_12_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_DMSO_01_sweep/run0001_sc_1us_5.8us_11_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_DMSO_01_sweep/run0001_sc_1us_5.8us_10_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_DMSO_01_sweep/run0001_sc_1us_5.8us_09_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_DMSO_01_sweep/run0001_sc_1us_5.8us_08_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_DMSO_01_sweep/run0001_sc_1us_5.8us_07_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_DMSO_01_sweep/run0001_sc_1us_5.8us_06_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_DMSO_01_sweep/run0001_sc_1us_5.8us_05_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_DMSO_01_sweep/run0001_sc_1us_5.8us_04_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_DMSO_01_sweep/run0001_sc_1us_5.8us_03_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_DMSO_01_sweep/run0001_sc_1us_5.8us_02_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_DMSO_01_sweep/run0001_sc_1us_5.8us_01_master_0.h5 $DARKS_D0 $DARKS_D1;
+
+sbatch convert_images_burst.sh EP_A07_01_sweep/run0001_sc_1us_5.8us_12_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_A07_01_sweep/run0001_sc_1us_5.8us_11_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_A07_01_sweep/run0001_sc_1us_5.8us_10_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_A07_01_sweep/run0001_sc_1us_5.8us_09_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_A07_01_sweep/run0001_sc_1us_5.8us_08_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_A07_01_sweep/run0001_sc_1us_5.8us_07_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_A07_01_sweep/run0001_sc_1us_5.8us_06_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_A07_01_sweep/run0001_sc_1us_5.8us_05_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_A07_01_sweep/run0001_sc_1us_5.8us_04_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_A07_01_sweep/run0001_sc_1us_5.8us_03_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_A07_01_sweep/run0001_sc_1us_5.8us_02_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_A07_01_sweep/run0001_sc_1us_5.8us_01_master_0.h5 $DARKS_D0 $DARKS_D1;
+
+
+sbatch convert_images_burst.sh EP_C04_01_sweep/run0001_sc_1us_5.8us_12_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_C04_01_sweep/run0001_sc_1us_5.8us_11_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_C04_01_sweep/run0001_sc_1us_5.8us_10_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_C04_01_sweep/run0001_sc_1us_5.8us_09_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_C04_01_sweep/run0001_sc_1us_5.8us_08_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_C04_01_sweep/run0001_sc_1us_5.8us_07_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_C04_01_sweep/run0001_sc_1us_5.8us_06_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_C04_01_sweep/run0001_sc_1us_5.8us_05_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_C04_01_sweep/run0001_sc_1us_5.8us_04_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_C04_01_sweep/run0001_sc_1us_5.8us_03_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_C04_01_sweep/run0001_sc_1us_5.8us_02_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_C04_01_sweep/run0001_sc_1us_5.8us_01_master_0.h5 $DARKS_D0 $DARKS_D1;
+
+
+sbatch convert_images_burst.sh EP_F11_01_sweep/run0001_sc_1us_5.8us_12_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_F11_01_sweep/run0001_sc_1us_5.8us_11_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_F11_01_sweep/run0001_sc_1us_5.8us_10_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_F11_01_sweep/run0001_sc_1us_5.8us_09_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_F11_01_sweep/run0001_sc_1us_5.8us_08_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_F11_01_sweep/run0001_sc_1us_5.8us_07_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_F11_01_sweep/run0001_sc_1us_5.8us_06_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_F11_01_sweep/run0001_sc_1us_5.8us_05_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_F11_01_sweep/run0001_sc_1us_5.8us_04_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_F11_01_sweep/run0001_sc_1us_5.8us_03_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_F11_01_sweep/run0001_sc_1us_5.8us_02_master_0.h5 $DARKS_D0 $DARKS_D1;
+sbatch convert_images_burst.sh EP_F11_01_sweep/run0001_sc_1us_5.8us_01_master_0.h5 $DARKS_D0 $DARKS_D1;
