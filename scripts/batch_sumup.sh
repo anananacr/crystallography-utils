7703889cb60ef651f46c050a000a8d3a9a7b0b52
Ana Rodrigues <anananacr@gmail.com>
2024-09-25 17:29:16 +0200

    sum up sequential frames

--- scripts/batch_sumup.sh
+++ scripts/batch_sumup.sh
@@ -1,26 +1,28 @@
 #!/bin/sh
 
-#SBATCH --partition=partition
+#SBATCH --partition=allcpu
 #SBATCH --time=2-00:00:00
 #SBATCH --requeue
 #SBATCH --nodes=1
-#SBATCH --mincpus=64
-#SBATCH --mem=10G
+#SBATCH --mincpus=20
+#SBATCH --mem=4G
 #SBATCH --nice=100
-#SBATCH --job-name  bb
-#SBATCH --output   /gpfs/exfel/exp/SPB/202425/p008396/scratch/rodria/error/bb-%N-%j.out
-#SBATCH --error   /gpfs/exfel/exp/SPB/202425/p008396/scratch/rodria/error/bb-%N-%j.err
+#SBATCH --job-name  batch_sum
+#SBATCH --output   /gpfs/exfel/exp/SPB/202425/p008396/scratch/rodria/error/sum-%N-%j.out
+#SBATCH --error   /gpfs/exfel/exp/SPB/202425/p008396/scratch/rodria/error/sum-%N-%j.err
 
 INPUT=$1
-PATH=$2
-OUTPUT=$3
+OUTPUT=$2
+N=$3
+H5_PATH=$4
 ROOT=/gpfs/exfel/exp/SPB/202425/p008396/scratch/rodria
 
 source /etc/profile.d/modules.sh
 module purge
-source /gpfs/exfel/exp/SPB/202425/p008396/scratch/rodria/crystallography-utils-env/bin/activate
+source /gpfs/cfel/user/rodria/software/crystallography-utils-env/bin/activate
 
-command="python ${ROOT}/crystallography-utils/scripts/sum_frames.py -i ${ROOT}/centered/${INPUT} -p ${PATH}-o ${ROOT}/powder/${OUTPUT}"
+#command="python ${ROOT}/crystallography-utils/scripts/sum_frames.py -i ${ROOT}/centered/${INPUT} -o ${ROOT}/powder/${OUTPUT} -n ${N} -p ${H5_PATH} -lt 0 -gt 1000 -e True"
+command="python ${ROOT}/crystallography-utils/scripts/sum_frames.py -i ${ROOT}/centered/${INPUT} -o ${ROOT}/powder/${OUTPUT} -n ${N} -p ${H5_PATH}"
 
 echo $command
 $command
--- scripts/sum_frames.py
+++ scripts/sum_frames.py
@@ -1,14 +1,14 @@
 import h5py
 import argparse
 import math
+import numpy as np
 import os
-## TO TEST
 
 def skip_lost_panel(data:np.array, lost_flag:int):
-    if len(np.where(panel_data<=lost_flag)[0])>5e5:
+    if len(np.where(data<=lost_flag)[0])>5.2e5:
         return True
     else:
-        returne False
+        return False
 
 def main():
     parser = argparse.ArgumentParser(
@@ -26,6 +26,7 @@ def main():
         "--path",
         type=str,
         action="store",
+        default ="/entry/data/data",
         help="data hdf5 path",
     )
     parser.add_argument(
@@ -33,7 +34,7 @@ def main():
         "--lost_flag",
         type=int,
         action="store",
-        default = -2e6
+        default = -1.15e8,
         help="lost packet value",
     )
     parser.add_argument(
@@ -42,7 +43,7 @@ def main():
         type=int,
         action="store",
         default = -1,
-        help="number of frames to sum, -1 the ",
+        help="number of frames to sum, -1 sum all of the frames ",
     )
     parser.add_argument(
         "-s",
@@ -50,7 +51,32 @@ def main():
         type=int,
         action="store",
         default=0,
-        help="starting frame, default is 0 ",
+        help="starting frame, default is 0.",
+    )
+    parser.add_argument(
+        "-lt",
+        "--mask_less_than",
+        type=int,
+        action="store",
+        default=None,
+        help="mask values less than a value in the output array, default is None which means not mask anything. Masked values are replaced by 0 (zero).",
+    )
+
+    parser.add_argument(
+        "-gt",
+        "--mask_greater_than",
+        type=int,
+        action="store",
+        default=None,
+        help="mask values greater than a value in the output array, default is None which means not mask anything. Masked values are replaced by 0 (zero).",
+    )
+    parser.add_argument(
+        "-e",
+        "--apply_exponential",
+        type=bool,
+        action="store",
+        default=False,
+        help="apply exponential to each number of the array, default is False which will not apply the exponential to each number of the array.",
     )
     parser.add_argument(
         "-o", "--output", type=str, action="store", help="output path in .h5"
@@ -59,7 +85,10 @@ def main():
     args = parser.parse_args()
 
     h5_data_path = args.path
+    print(h5_data_path)
+    h5_data_path = "/entry_1/data_1/data"
     starting_frame = args.starting_frame
+    print(h5_data_path)
 
     with h5py.File(args.input, "r") as f:
         if args.number_of_frames==-1:
@@ -69,7 +98,8 @@ def main():
         
         
         data_shape=f[h5_data_path].shape[1:]
-        acc_data=np.zeros(data_shape, dtype=np.int32)
+        print((1,*data_shape))
+        acc_data=np.zeros((1,*data_shape), dtype=np.float32)
         if len(data_shape)>1:
             count=[0,0,0,0,0,0,0,0]
             print("I understand this is a VDS file.")          
@@ -78,11 +108,11 @@ def main():
                     panel_data = np.array(frame[panel_id,:], dtype=np.int32)
                     # Skip lost packets
                     if not skip_lost_panel(panel_data, args.lost_flag):
-                        acc_data[panel_id]+=panel_data
+                        acc_data[0,panel_id,:]+=panel_data
                         count[panel_id]+=1
 
             for panel_id in range(data_shape[0]):
-                panel_data[panel_id]/=count[panel_id]               
+                acc_data[0,panel_id,:]/=count[panel_id]               
         else:
             count=0
             for frame in f[h5_data_path][starting_frame:starting_frame+n_frames]:
@@ -90,8 +120,18 @@ def main():
                 count+=1
             acc_data/=count
 
+    if args.apply_exponential:
+        acc_data=np.exp(acc_data)
+
+    if args.mask_less_than is not None:
+        acc_data[np.where(acc_data<args.mask_less_than)]=0
+    
+    if args.mask_greater_than is not None:
+        acc_data[np.where(acc_data>args.mask_greater_than)]=0
+        print("hi")
+
     with h5py.File(args.output, "w") as f:
-        f.create_dataset(f"{h5_data_path}", data=acc_data)
+        f.create_dataset(f"{h5_data_path}", data=(acc_data).astype(np.int32))
 
 
 if __name__ == "__main__":
